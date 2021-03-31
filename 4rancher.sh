77c4633f86af0ec86bab535a88c0417e816f1959
evesan <everardo.ipn@gmail.com>
2021-03-31 00:08:21 -0600

    refact: updates script

--- 4rancher.sh
+++ 4rancher.sh
@@ -16,11 +16,22 @@ helm repo update
 kubectl create namespace cattle-system
 
 ##
-##  USANDO UN CA propio así como los certificados propios del gir.dev.sat.gob.mx
-## Añadimos certificados.
-kubectl -n cattle-system create secret tls tls-rancher-ingress --cert=gir.cer --key=gir.key
-kubectl -n cattle-system create secret generic tls-ca --from-file=cacerts.pem=./CA.pem
-## Instalamos rancher
-helm install rancher rancher-stable/rancher --namespace cattle-system --set hostname=gir.dev.sat.gob.mx --set ingress.tls.source=secret --set privateCA=true
-## Esperamos al deploy
+##  USANDO CERTIFICADOS DE RANCHER
+## Instalamos cert-manager
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.4/cert-manager.crds.yaml
+kubectl create namespace cert-manager
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --version v1.0.4
+## Verify
+kubectl get pods --namespace cert-manager
+
+## instalamos rancher
+helm install rancher rancher-stable/rancher \
+  --namespace cattle-system \
+  --set hostname=rancher.my.org
+## Verify
 kubectl -n cattle-system rollout status deploy/rancher
